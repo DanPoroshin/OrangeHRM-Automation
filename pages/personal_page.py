b5ae21c12ba7bec69758468a3e5cd7ba582fcf09
Данил Порошин <danil@MacBook-Air-Danil.local>
2024-07-05 14:33:35 +0500

    Finished positive testing for personal info

--- pages/personal_page.py
+++ pages/personal_page.py
@@ -1,6 +1,7 @@
 import allure
 from base.base_page import BasePage
 from config.links import Links
+from fields.personal_page_fields import PersonalPageFields as Fields
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Keys
 
@@ -8,26 +9,82 @@ from selenium.webdriver import Keys
 class PersonalPage(BasePage):
 
     PAGE_URL = Links.PERSONAL_PAGE
-
-    FIRST_NAME_FIELD = ('xpath', '//input[@name="firstName"]')
-    SAVE_BUTTON = ('xpath', '(//button[@type="submit"])[1]')
-    SPINNER = ('xpath', '//div[@class="oxd-loading-spinner"]')
-
     def change_name(self, name):
         with allure.step(f"Change name to {name}"):
 
-            first_name_field = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME_FIELD))
+            first_name_field = self.wait.until(EC.element_to_be_clickable(Fields.FIRST_NAME_FIELD))
             first_name_field.send_keys(Keys.CONTROL + "a")
             first_name_field.send_keys(Keys.BACKSPACE)
             first_name_field.send_keys(name)
-            self.name = name
 
-    @allure.step("Click on 'Save' button")
-    def save_changes(self):
-        self.wait.until(EC.element_to_be_clickable(self.SAVE_BUTTON)).click()
+    def change_middle_name(self, middle_name):
+        with allure.step(f"Change middle name to {middle_name}"):
 
-    @allure.step("Check that name was changed")
+            middle_name_field = self.wait.until(EC.element_to_be_clickable(Fields.MIDDLE_NAME_FIELD))
+            middle_name_field.send_keys(Keys.CONTROL + "a")
+            middle_name_field.send_keys(Keys.BACKSPACE)
+            middle_name_field.send_keys(middle_name)
+    
+    def change_last_name(self, last_name):
+        with allure.step(f"Change last name to {last_name}"):
+
+            last_name_field = self.wait.until(EC.element_to_be_clickable(Fields.LAST_NAME_FIELD))
+            last_name_field.send_keys(Keys.CONTROL + "a")
+            last_name_field.send_keys(Keys.BACKSPACE)
+            last_name_field.send_keys(last_name)
+
+    def change_employee_id(self, id):
+        with allure.step(f"Change employee id to {id}"):
+            employee_id_field = self.wait.until(EC.element_to_be_clickable(Fields.EMPLOYEE_ID_FIELD))
+            employee_id_field.send_keys(Keys.CONTROL + "a")
+            employee_id_field.send_keys(Keys.BACKSPACE)
+            employee_id_field.send_keys(id)
+
+    def change_license_id(self, license_id):
+        with allure.step(f"Change license id to {license_id}"):
+            license_id_field = self.wait.until(EC.element_to_be_clickable(Fields.DRIVER_LICENSE_NUMBER_FIELD))
+            license_id_field.send_keys(Keys.CONTROL + "a")
+            license_id_field.send_keys(Keys.BACKSPACE)
+            license_id_field.send_keys(license_id)
+
+    def change_license_expiry_date_by_input(self, date):
+        with allure.step(f"Change license expiry date to {date}"):
+            expiry_field = self.wait.until(EC.element_to_be_clickable(Fields.DRIVER_LICENSE_EXPIRY_DATE_FIELD))
+            expiry_field.send_keys(Keys.CONTROL + "a")
+            expiry_field.send_keys(Keys.BACKSPACE)
+            expiry_field.send_keys(date)
+
+    def change_nationality(self, nationality):
+        with allure.step(f"Change nationality to {nationality}"):
+            nationality_field = self.wait.until(EC.element_to_be_clickable(Fields.NATIONALITY_FIELD))
+            nationality_field.click()
+            self.wait.until(EC.element_to_be_clickable(('xpath', '//span[text() = "' + nationality + '"]'))).click()
+
+    def change_marital_status(self, status):
+        with allure.step(f"Change marital status to {status}"):
+            marital_status_field = self.wait.until(EC.element_to_be_clickable(Fields.MARITAL_STATUS_FIELD))
+            marital_status_field.click()
+            self.wait.until(EC.element_to_be_clickable(('xpath', '//span[text() = "' + status + '"]'))).click()
+
+    def change_date_of_birth_by_input(self, date):
+        with allure.step(f"Change date of birth to {date}"):
+            date_of_birth_field = self.wait.until(EC.element_to_be_clickable(Fields.DATE_OF_BIRTH_FIELD))
+            date_of_birth_field.send_keys(Keys.CONTROL + "a")
+            date_of_birth_field.send_keys(Keys.BACKSPACE)
+            date_of_birth_field.send_keys(date)
+
+    def change_gender(self, gender):
+        with allure.step(f"Change gender to {gender}"):
+            self.wait.until(EC.element_to_be_clickable(('xpath', '//label[text() = "' + gender + '"]'))).click()
+
+    @allure.step("Click on 'Save' button for personal info")
+    def save_personal_changes(self):
+        self.wait.until(EC.element_to_be_clickable(Fields.PERSONAL_SAVE_BUTTON)).click()
+
+    @allure.step("Get error message")
+    def get_error_message(self):
+        return self.wait.until(EC.visibility_of_element_located(Fields.ERROR_MESSAGE)).text
+    
+    @allure.step("Check that settings applied successfully")
     def is_changes_saved(self):
-        self.wait.until(EC.invisibility_of_element_located(self.SPINNER))
-        self.wait.until(EC.visibility_of_element_located(self.FIRST_NAME_FIELD))
-        self.wait.until(EC.text_to_be_present_in_element_value(self.FIRST_NAME_FIELD, self.name))
+        self.wait.until(EC.invisibility_of_element_located(Fields.SPINNER))
--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -5,9 +5,9 @@ import pytest
 @allure.feature("Profile")
 class TestProfileFeature(BaseTest):
 
-    @allure.title("Change profile name")
+    @allure.title("Change profile first name")
     @allure.severity(allure.severity_level.NORMAL)
-    @pytest.mark.smoke
+    @pytest.mark.positive
     def test_change_profile_name(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
@@ -17,6 +17,156 @@ class TestProfileFeature(BaseTest):
         self.dashboard_page.click_my_info()
         self.personal_page.is_opened()
         self.personal_page.change_name("Alexander")
-        self.personal_page.save_changes()
+        self.personal_page.save_personal_changes()
         self.personal_page.is_changes_saved()
         self.personal_page.make_screenshot("profile_name_changed")
+
+
+    @allure.title("Change profile middle name")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.positive
+    def test_change_profile_middle_name(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_middle_name("Petrov")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_middle_name_changed")
+
+    @allure.title("Change profile last name")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.positive
+    def test_change_profile_last_name(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_last_name("Ivanov")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_last_name_changed")
+
+
+    @allure.title("Change profile employee id")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.positive
+    def test_change_employee_id(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_employee_id("5768")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_employee_id_changed")
+
+
+    @allure.title("Change profile license id")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.positive
+    def test_change_license_id(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_license_id("123456")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_license_id_changed")
+
+
+    @allure.title("Change profile license expiry date")
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.positive
+    def test_change_license_expiry_date(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_license_expiry_date_by_input("2025-01-01")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_license_expiry_date_changed")
+
+
+    @allure.title("Change profile nationality")
+    @allure.severity(allure.severity_level.MINOR)
+    @pytest.mark.positive
+    def test_change_nationality(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_nationality("Russian")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_nationality_changed")
+
+
+    @allure.title("Change profile marital status")
+    @allure.severity(allure.severity_level.MINOR)
+    @pytest.mark.positive
+    def test_change_marital_status(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_marital_status("Married")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_marital_status_changed")
+
+    @allure.title("Change profile date of birth")
+    @allure.severity(allure.severity_level.MINOR)
+    @pytest.mark.positive
+    def test_change_profile_date_of_birth(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_date_of_birth_by_input("1990-01-01")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_date_of_birth_changed")
+    
+    @allure.title("Change profile gender")
+    @allure.severity(allure.severity_level.MINOR)
+    @pytest.mark.positive
+    def test_change_profile_gender(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_my_info()
+        self.personal_page.is_opened()
+        self.personal_page.change_gender("Male")
+        self.personal_page.save_personal_changes()
+        self.personal_page.is_changes_saved()
+        self.personal_page.make_screenshot("profile_gender_changed")
